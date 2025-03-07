bcf511c4818b396b8cb5ff1fe5ae2573f46d7e90
Moin Munna <mnddnmunna@gmail.com>
2025-03-08 02:47:31 +0600

    added a p tag

--- templates/admin_submenu_markup.php
+++ templates/admin_submenu_markup.php
@@ -1,5 +1,6 @@
 <div class="wrap">
     <h1>Mini Cart Shortcode Styles</h1>
+    <p>Use this shortcode &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <strong> [cmcw_mini_cart] </strong></p>
     <form method="post" action="options.php">
         <?php
         settings_fields('cmcw_settings');
