9db8959ffa65c79915c8013e376d51c27df23587
Moin Munna <mnddnmunna@gmail.com>
2025-03-08 12:27:52 +0600

    version update

--- complete-mini-cart-for-woocommerce.php
+++ complete-mini-cart-for-woocommerce.php
@@ -4,7 +4,7 @@
  * Plugin Name:       Mini Cart for WooCommerce
  * Plugin URI:        https://example.com/plugins/the-basics/
  * Description:       This plugin adds a mini cart feature to your WooCommerce store. An Elementor Widget, a Block and a shortcode. All that you needed in one simple plugin.
- * Version:           1.0.0
+ * Version:           1.0.1
  * Requires at least: 6.0
  * Requires PHP:      7.4
  * Author:            Moin Munna
